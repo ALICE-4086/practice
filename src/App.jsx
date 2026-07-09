2963f0c499d2a72a5299ec3cfe6ba52a87180d00
ALICE-4086 <savech7@gmail.com>
2026-07-09 10:42:36 +0900

    test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from './components/Banner.jsx'
+import Board from './components/Board.jsx'
+import Profile from './components/Profile.jsx'
+
+export default function App() {
+  return (
+    <div className="app-shell">
+      <Banner />
+      <Board />
+      <Profile />
+    </div>
+  )
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+export default function Profile() {
+  return (
+    <section className="profile">
+      <div className="profile-avatar">홍</div>
+      <div className="profile-info">
+        <h3>홍길동</h3>
+        <p>010-1234-5678</p>
+        <p>abcd@abcd.com</p>
+      </div>
+    </section>
+  )
+}
